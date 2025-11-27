c137aaff9d08fc971aaa945ce0355a0ac6361baf
Adamyx <cseh.adam@kreativszolnok.hu>
2025-11-27 11:36:16 +0100

    1

---
+++ kviz.js
@@ -0,0 +1,58 @@
+const questions = [
+    { 
+        subject: "matematika",
+        question: "Mennyi a sin(30°)?",
+        answers: ["0.5", "0.866", "1"],
+        correct: 0
+    },
+    { 
+        subject: "matematika",
+        question: "Mennyi a cos(60°)?",
+        answers: ["0.5", "0.75", "1"],
+        correct: 0
+    },
+    { 
+        subject: "matematika",
+        question: "Mennyi a tan(45°)?",
+        answers: ["0", "1", "√3"],
+        correct: 1
+    },
+
+    { 
+        subject: "történelem",
+        question: "Mikor tört ki az 1848-as forradalom Magyarországon?",
+        answers: ["március 15.", "április 14.", "szeptember 29."],
+        correct: 0
+    },
+    { 
+        subject: "történelem",
+        question: "Ki írta a 12 pontot?",
+        answers: ["Petőfi Sándor", "Irinyi József", "Kossuth Lajos"],
+        correct: 1
+    },
+    { 
+        subject: "történelem",
+        question: "Hol történt a forradalom egyik első nagy tömegrendezvénye?",
+        answers: ["Nemzeti Múzeum", "Lánchíd", "Országház"],
+        correct: 0
+    },
+
+    { 
+        subject: "irodalom",
+        question: "Hány színből áll Az ember tragédiája?",
+        answers: ["15", "17", "20"],
+        correct: 0
+    },
+    { 
+        subject: "irodalom",
+        question: "Ki írta Az ember tragédiáját?",
+        answers: ["Arany János", "Madách Imre", "Vörösmarty Mihály"],
+        correct: 1
+    },
+    { 
+        subject: "irodalom",
+        question: "Hogyan hívják Az ember tragédiája főszereplőjét?",
+        answers: ["Ádám", "Lucifer", "Mindkettő fontos szereplő"],
+        correct: 2
+    }
+];
